a883510ec82b1602bcdc185922ffed17e6c29964
MarHarFS <marko.harjumaki@gmail.com>
2021-10-27 17:12:55 +0300

    markon check ja lista

--- src/MarkonKansio/Markon.js
+++ src/MarkonKansio/Markon.js
@@ -6,7 +6,20 @@ function Markon() {
 
   return (
     <>
-    {questions.map((q) => <div> {q.question} </div>)}
+      {questions.map((q) =>
+        <table key={q.id}>
+          <thead>
+            <tr><th>{q.question}</th></tr>
+          </thead>
+          <tbody>
+            {q.answer.map(a =>
+              <tr key={a.id}><td >
+                <input type="checkbox" id={a.id} ></input>{a.answ}
+              </td></tr>
+            )}
+          </tbody>
+        </table>
+      )}
     </>
   )
 }
