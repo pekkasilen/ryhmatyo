43ea07032e49b3b72356d5ed8e874af4e38be6ec
Ville <nissinen.vj@gmail.com>
2021-10-27 19:31:22 +0300

    kommentti

--- src/VillenKansio/Tentti_Ville.js
+++ src/VillenKansio/Tentti_Ville.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Data_Ville from "./Data_Ville.json"
 
 function Tentti_Ville() {
+
+    // Tässä pitäisi ottaa koppi datasta ja saada se sidottua tilaan eikä suoraan renderöidä.
+
     return (
         <div>
            {Data_Ville.map(item => 
