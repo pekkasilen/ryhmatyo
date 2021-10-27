5075d6efb880e3bbb02db881de16e99dd8caa64e
Kristian Asti <kristian.asti@student.tut.fi>
2021-10-27 15:43:13 +0300

    Kristianin tenttifilu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import React, {useState} from 'react';
 import { useEffect } from 'react';
 import a from './data'
+
+
+
 function App() {
   
   // TESTI: Kristian testailee
---
+++ src/Tentti_Kristian.js
@@ -0,0 +1,23 @@
+/*  Kristian Asti
+    27.10.2021
+    Full Stack 2020-21
+
+    Tenttisovellus-ryhmätyö, Kristianin toteutus
+*/
+
+import React, { useState, useEffect } from 'react';
+
+const axios = require('axios');
+
+
+function Tentti_Kristian() {
+
+
+
+}
+
+
+
+return "";
+
+export default Tentti_Kristian;
