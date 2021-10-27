6457d412153163c1e2319ec32f5037d731361870
Ville <nissinen.vj@gmail.com>
2021-10-27 19:25:39 +0300

    Villen modauksia

--- src/VillenKansio/Tentti_Ville.js
+++ src/VillenKansio/Tentti_Ville.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Data_Ville from "./Data_Ville.json"
 
 function Tentti_Ville() {
     return (
         <div>
-            Renderiä kehiin
+           {Data_Ville.map(item => 
+           <div className="QuestionCardView">
+               {item.kysymys}
+               <div className="Container">
+                   {item.väittämät.map(item => 
+                   <div className="Question">
+                       <input checked={item.CB} type="checkbox"></input>
+                       {item.väittämä}
+                    </div>)}
+                </div>
+            </div>)}
         </div>
     )
 }
